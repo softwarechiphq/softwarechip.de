392a53d7aa3048ceb1c6be8454ea9d8f962c2079
Pratham <Pratham.Ghaywat@outlook.com>
2026-04-04 08:03:50 +0000

    Techstack overhaul. Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({
+  plugins: [tailwindcss(), reactRouter()],
+  resolve: {
+    tsconfigPaths: true,
+  },
+});
